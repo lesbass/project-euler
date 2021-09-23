987edb2b8923c530be0d12e122bd521af758e440
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-09-23 23:19:55 +0200

    Linting

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,2 @@
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.0")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt"  % "2.4.3")
--- src/main/scala/ProblemSolver.scala
+++ src/main/scala/ProblemSolver.scala
@@ -2,16 +2,18 @@ import scala.language.postfixOps
 
 object ProblemSolver {
 
-  def problem1(n: Int): Int = {
-    (0 until n).reduce((sum, a) => sum + (if (a % 3 == 0 || a % 5 == 0) a else 0))
-  }
+  def problem1(n: Int): Int =
+    (0 until n).reduce((sum, a) =>
+      sum + (if (a % 3 == 0 || a % 5 == 0) a else 0)
+    )
 
   def problem2(n: Int): Int = {
 
     def run(limit: Int)(prevNumber: Int, currNumber: Int): Int = {
       def checkEven(n: Int): Int = if (n % 2 == 0) n else 0
 
-      def runInside(sum: Int): Int = checkEven(sum) + runWithLimit(currNumber, sum)
+      def runInside(sum: Int): Int =
+        checkEven(sum) + runWithLimit(currNumber, sum)
 
       if (currNumber > limit) 0 else runInside(prevNumber + currNumber)
     }
